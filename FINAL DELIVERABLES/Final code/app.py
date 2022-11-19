576557f4e9dded0b5b521eb1200fbe0bf82720ce
ST-SHARAN <85843551+ST-SHARAN@users.noreply.github.com>
2022-11-19 12:00:49 +0530

    Create app.py

---
+++ FINAL DELIVERABLES/Final code/app.py
@@ -0,0 +1,214 @@
+import os
+import pandas as pd
+import tensorflow as tf
+from werkzeug.utils import secure_filename
+from flask import Flask, render_template, url_for, redirect, request
+from flask_sqlalchemy import SQLAlchemy #ORM
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
+from flask_wtf import FlaskForm #flask form
+from wtforms import StringField, PasswordField, SubmitField, IntegerField
+from wtforms.validators import InputRequired, Length, ValidationError
+from flask_bcrypt import Bcrypt
+from werkzeug.utils import secure_filename
+from werkzeug.datastructures import  FileStorage
+import operator
+import cv2 # opencv library
+import matplotlib.pyplot as plt #image processing
+import matplotlib.image as mpimg #image processing
+import numpy as np
+from tensorflow.keras.models import load_model
+import mediapipe as mp
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__,template_folder="templates")
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SECRET_KEY'] = 'thisisasecretkey'
+model=load_model('gesture.h5')
+print("Loaded model from disk")
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False, unique=True)
+    password = db.Column(db.String(80), nullable=False)
+
+class RegisterForm(FlaskForm):
+    username = StringField("Username : ",validators=[
+                           InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Enter your username"})
+
+    password = PasswordField("Password : ",validators=[
+                             InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Enter your password"})
+
+
+    submit = SubmitField('Signup')
+
+def validate_username(self, username):
+    existing_user_username = User.query.filter_by(username=username.data).first()
+    if existing_user_username:
+        raise ValidationError('That username already exists. Please choose a different one.')
+
+    
+class LoginForm(FlaskForm):
+    username = StringField(validators=[
+                           InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
+
+    password = PasswordField(validators=[
+                             InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
+
+    submit = SubmitField('Login')
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            if bcrypt.check_password_hash(user.password, form.password.data):
+                login_user(user)
+                return redirect(url_for('dashboard'))
+    return render_template('login.html', form=form)
+
+
+@app.route('/dashboard', methods=['GET', 'POST'])
+@login_required
+def dashboard():
+    return render_template('dashboard.html')
+
+
+@app.route('/logout', methods=['GET', 'POST'])
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data)
+        new_user = User(username=form.username.data, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('login'))
+
+    return render_template('register.html', form=form)
+
+
+@app.route('/predict',methods=['GET', 'POST'])# route to show the predictions in a web UI
+@login_required
+def launch():
+    if request.method == 'POST':
+        print("inside image")
+        f = request.files['image']
+
+        basepath = os.path.dirname(__file__)
+        file_path = os.path.join(basepath, 'uploads', secure_filename(f.filename))
+        f.save(file_path)   
+        print(file_path)
+        cap = cv2.VideoCapture(0)
+        while True:
+            _, frame = cap.read() #capturing the video frame values
+            # Simulating mirror image
+            frame = cv2.flip(frame, 1)
+            
+            # Got this from collect-data.py
+            # Coordinates of the ROI
+            x1 = int(0.5*frame.shape[1]) 
+            y1 = 10
+            x2 = frame.shape[1]-10
+            y2 = int(0.5*frame.shape[1])
+            # Drawing the ROI
+            # The increment/decrement by 1 is to compensate for the bounding box
+            cv2.rectangle(frame, (x1-1, y1-1), (x2+1, y2+1), (255,0,0) ,1)
+            # Extracting the ROI
+            roi = frame[y1:y2, x1:x2]
+            
+            # Resizing the ROI so it can be fed to the model for prediction
+            roi = cv2.resize(roi, (64, 64)) 
+            roi = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+            _, test_image = cv2.threshold(roi, 120, 255, cv2.THRESH_BINARY)
+            cv2.imshow("test", test_image)
+            # Batch of 1
+            result = model.predict(test_image.reshape(1, 64, 64, 1))
+            prediction = {'ZERO': result[0][0], 
+                          'ONE': result[0][1], 
+                          'TWO': result[0][2],
+                          'THREE': result[0][3],
+                          'FOUR': result[0][4],
+                          'FIVE': result[0][5]}
+            # Sorting based on top prediction
+            prediction = sorted(prediction.items(), key=operator.itemgetter(1), reverse=True)
+            
+            # Displaying the predictions
+            cv2.putText(frame, prediction[0][0], (10, 120), cv2.FONT_HERSHEY_PLAIN, 1, (0,255,255), 1)    
+            cv2.imshow("Frame", frame)
+            
+            #loading an image
+            image1=cv2.imread(file_path)
+            if prediction[0][0]=='TWO':
+                print("Flip : 2 - gesture")
+                img = cv2.flip(image1, -1)
+                cv2.imshow('Flipped image',img)
+                key=cv2.waitKey(3000)
+                if (key & 0xFF) == ord("2"):
+                    cv2.destroyWindow("Rectangle")
+
+            elif prediction[0][0]=='FIVE':
+                print("Rectange: 5 - gesture")
+                cv2.rectangle(image1, (480, 170), (650, 420), 4444)
+                cv2.imshow("Rectangle", image1)
+                
+                key=cv2.waitKey(3000)
+                if (key & 0xFF) == ord("5"):
+                    cv2.destroyWindow("Rectangle")
+
+            elif prediction[0][0]=='THREE':
+                print("Blured : 3 - gesture")
+                blurred = cv2.GaussianBlur(image1, (21, 21), 0)
+                cv2.imshow("Blurred", blurred)
+                key=cv2.waitKey(3000)
+                if (key & 0xFF) == ord("3"):
+                    cv2.destroyWindow("Blurred")
+
+            elif prediction[0][0]=='FOUR':
+                print("400x400 : 4 - gesture")
+                resized = cv2.resize(image1, (400, 400))
+                cv2.imshow("Fixed Resizing", resized)
+                key=cv2.waitKey(3000)
+                if (key & 0xFF) == ord("4"):
+                    cv2.destroyWindow("Fixed Resizing")
+            
+            
+            interrupt = cv2.waitKey(10)
+            if interrupt & 0xFF == 27: # esc key
+                break
+                
+         
+        cap.release()
+        cv2.destroyAllWindows()
+    return render_template("home.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port = 5000)
